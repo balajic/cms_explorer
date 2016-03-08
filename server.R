a01b3be7bc4bffec1b2f0a35e04693831c5af16d
Balaji Chellappa <balaji.srgm@gmail.com>
2016-03-08 01:15:30 -0500

    Ver 0.1: Bases on simple CSV file

--- server.R
+++ server.R
@@ -8,5 +8,14 @@ library(DT)
 library(jsonlite)
 
 function(input,output,session){
-  
+  tabCMS <- eventReactive(input$goButton, {
+    disable("goButton")
+    out1 <- read.csv("HCP.csv")
+    enable("goButton")
+    out1
+  })
+
+  output$outCMS <- renderDataTable({
+    datatable(tabCMS(),options = list(), rownames = F, filter = 'top')
+  })
 }
--- ui.R
+++ ui.R
@@ -1,20 +1,33 @@
 # Sriramajayam
+#install.packages('dplyr')
+# colnames(tab)
 library(shiny)
 library(shinydashboard)
 library(shinyjs)
 library(DT)
 library(jsonlite)
+library(dplyr)
+tabHCP <- read.csv("HCP.csv",header=TRUE)
+#colnames(tabHCP)
+#unique(tabHCP[,24])
 
-companyList <- c("","Purdue Pharma L.P","Merck","Novartis")
-drugList   <- c("",'Oxycontin',"Butrans","Hysingla ER")
+#companyList <- unique(tabHCP[,24])
+companyList <- distinct(tabHCP["Submitting_Applicable_Manufacturer_or_Applicable_GPO_Name"],Submitting_Applicable_Manufacturer_or_Applicable_GPO_Name)
+
+#drugList   <-  unique(tabHCP[,47])
+drugList   <-  distinct(tabHCP["Name_of_Associated_Covered_Drug_or_Biological1"],Name_of_Associated_Covered_Drug_or_Biological1)
 
 dashboardPage(
   dashboardHeader(title = "Filter"),
   dashboardSidebar(
     useShinyjs(),
-    h3("Filter Goes here")
+    h3("Filter Goes here"),
+    selectInput("company", "Company Name", c(companyList), selected = NULL, multiple = FALSE, selectize = TRUE, width = NULL, size = NULL),
+    selectInput("drug", "Drug List", drugList, selected = NULL, multiple = FALSE, selectize = TRUE, width = NULL, size = NULL),
+    actionButton("goButton","Query")
   ),
   dashboardBody(
-    h3("2014 CMS Open Payments Data explorer")
+    h3("2014 CMS Open Payments Data explorer"),
+    dataTableOutput("outCMS")
   )
 )
